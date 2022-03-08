91360d7178cc142022f147cc268ab7f2c052ffd9
Valentino Carneiro <37012871+xvalme@users.noreply.github.com>
2022-03-08 13:26:12 +0000

    Update MEM_WB.v

--- LEGv8_Pipelined_CPU/CompletedModules/MEM_WB.v
+++ LEGv8_Pipelined_CPU/CompletedModules/MEM_WB.v
@@ -4,23 +4,27 @@ module mem_wb (
 	input wire [63:0] read_data,
 	input wire [63:0] alu_result,
 	input wire [4:0] write_reg,
-	input wire [31:0] Instruction; //Just for testing
+	input wire [31:0] instruction; //Just for testing
 	//WB
 	input wire regWrite,
 	input wire memtoReg,
 
 	//Outputs
+	output reg [31:0] Instruction;
+	
 	output reg [63:0] Read_data,
 	output reg [63:0] Alu_result,
 	output reg [4:0] Write_reg,
 	//WB
 	output reg RegWrite,
 	output reg MemtoReg
+	
 
 );
 
 always @(posedge(clock)) begin
 
+	Instruction = instruction;
 	Read_data = read_data;
 	Alu_result = alu_result;
 	Write_reg = write_reg;
